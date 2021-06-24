831ae684d5c4d0304fbb6dd61064ed649fc5f0fb
Rishabh Pandey <62792790+RishabhMech@users.noreply.github.com>
2021-06-25 01:53:53 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -6,9 +6,6 @@
 
 #importing errored libraries
 import nltk
-#from chatterbot import utils
-#from chatterbot.tagging import PosHypernymTagger
-#from chatterbot.storage.storage_adapter import StorageAdapter
 
 
 import streamlit as st
@@ -20,34 +17,47 @@ from chatterbot.storage import StorageAdapter
 import json
 
 #get_text is a simple function to get user input from text_input
-def get_text():
-    input_text = st.text_input("You: ","So, what's in your mind")
-    return input_text
+class stream50():
 
-st.sidebar.title("NLP Bot")
-st.title("""
-NLP Bot  
-NLP Bot is an NLP conversational chatterbot. Initialize the bot by clicking the "Initialize bot" button. 
-""")
-#Preprocess input
-bot = ChatBot('Buddy', read_only = True, storage_adapter='chatterbot.storage.SQLStorageAdapter', logic_adapters = [
-                 {
-                     'import_path': 'chatterbot.logic.BestMatch', #'chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.TimeLogicAdapter'
-                     'default_response': 'I am sorry, I do not understand. I am still learning. Please contact \'Team chatbot@hd.com\' for further assistance!',
-                     'maximum_similarity_threshold': 0.90
-                 }
-             ], preprocessors=['chatterbot.preprocessors.clean_whitespace', 'chatterbot.preprocessors.unescape_html', 'chatterbot.preprocessors.convert_to_ascii'],                database_uri='sqlite:///database.sqlite3')
-
-ind = 1
-if st.sidebar.button('Initialize bot'):
-    trainer= ChatterBotCorpusTrainer(bot)
-    trainer.train('chatterbot.corpus.english')
-    st.title("Your bot is ready to talk to you")
-    ind = ind +1
+    def __init__(self):
+        #self.input_path=frame  
+        #self.weight_file_path=r"Downloads\weights_YOLO/yolov3_training_final.weights" 
         
-user_input = get_text()
-if True:
-    st.text_area("Bot:", value=bot.get_response(user_input), height=200, max_chars=None, key=None)
-else:
-    st.text_area("Bot:", value="Please start the bot by clicking sidebar button", height=200, max_chars=None, key=None)
 
+    def get_text():
+        input_text = st.text_input("You: ","So, what's in your mind")
+        return input_text
+
+    def process():
+        st.sidebar.title("NLP Bot")
+        st.title("""
+        NLP Bot  
+        NLP Bot is an NLP conversational chatterbot. Initialize the bot by clicking the "Initialize bot" button. 
+        """)
+        #Preprocess input
+        bot = ChatBot('Buddy', read_only = True, storage_adapter='chatterbot.storage.SQLStorageAdapter', logic_adapters = [
+                        {
+                            'import_path': 'chatterbot.logic.BestMatch', #'chatterbot.logic.MathematicalEvaluation', 'chatterbot.logic.TimeLogicAdapter'
+                            'default_response': 'I am sorry, I do not understand. I am still learning. Please contact \'Team chatbot@hd.com\' for further assistance!',
+                            'maximum_similarity_threshold': 0.90
+                        }
+                    ], preprocessors=['chatterbot.preprocessors.clean_whitespace', 'chatterbot.preprocessors.unescape_html', 'chatterbot.preprocessors.convert_to_ascii'],                database_uri='sqlite:///database.sqlite3')
+        
+        ind = 1
+        if st.sidebar.button('Initialize bot'):
+            trainer= ChatterBotCorpusTrainer(bot)
+            trainer.train('chatterbot.corpus.english')
+            st.title("Your bot is ready to talk to you")
+            ind = ind +1
+                
+        user_input = get_text()
+        if True:
+            st.text_area("Bot:", value=bot.get_response(user_input), height=200, max_chars=None, key=None)
+        else:
+            st.text_area("Bot:", value="Please start the bot by clicking sidebar button", height=200, max_chars=None, key=None)
+        
+# object initialization
+stream50_= stream50()
+
+if __name__ == "__main__":
+    stream50_.process()
